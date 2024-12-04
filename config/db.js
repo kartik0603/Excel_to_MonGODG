666554eae732ac7a49d4b3c135464490ac57e8f1
kartik0603 <kartikhirapara800@gmail.com>
2024-12-04 16:12:18 +0530

    excel to MOngo

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   const connection = await mongoose.connect(
-    "mongodb+srv://kartikhirapara800:1bngON3FMlQlC0cn@excelploader.juskv.mongodb.net/?retryWrites=true&w=majority&appName=excelploader"
+    process.env.MONGO_URI,
   );
   console.log(`MongoDB Connected`);
 };
